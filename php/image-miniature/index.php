47494bb853f34e773dab78272be34ad6a072204c
Ngoma M. Fortuna <ngomafortuna@gmail.com>
2025-04-05 23:28:48 +0100

    add test php image miniature

---
+++ php/image-miniature/index.php
@@ -0,0 +1,67 @@
+<style>
+    img {padding: 10px; border-radius: 5px;}
+</style>
+<?php
+
+use CoffeeCode\Cropper\Cropper;
+
+require __DIR__ . "/vendor/autoload.php";
+
+echo "upload_max_filesize: " . ini_get("upload_max_filesize");
+echo "<hr/>";
+
+echo "max_file_uploads: " . ini_get("max_file_uploads");
+echo "<hr/>";
+
+echo "post_max_size: " . ini_get("post_max_size");
+echo "<hr/>";
+
+
+
+
+$cropper = new Cropper("images/cachen");
+
+$arrCache = [];
+
+$images = [
+    'prim' => 'img1.jpg',
+    'segu' => 'img2.jpg',
+    'terc' => 'img3.jpg',
+    'quar' => 'img4.jpg',
+    'quin' => 'img5.jpg',
+];
+
+foreach($images as $index => $val) {
+    // $arrCache[$index] = $cropper->make("images/newimg/{$val}", 300, 300);
+}
+
+// $cropper->flush();
+
+foreach($arrCache as $val):
+?>
+
+    <!-- <img src="<?= $val ?>"> -->
+
+<?php 
+endforeach;
+
+// die();
+
+$cropper = new Cropper("images/cache");
+
+for($img = 1; $img < 5; $img++):
+?>
+
+    <div>
+        <!-- <img style="width: 100%;" src="images/img<?= $img ?>.jpg" alt="img<?= $img ?>.jpg"> -->
+        <img src="<?= $cropper->make("images/img{$img}.jpg", 800, 400) ?>" alt="img<?= $img ?>.jpg">     
+    </div>
+
+<?php 
+endfor; 
+
+
+// $cropper->flush("img1.jpg");
+// $cropper->flush();
+
+?>
